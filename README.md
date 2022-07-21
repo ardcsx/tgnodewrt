9b511aaf75100bb6ff50cb40ce6f8bdda28d1ac6
ardcs <ardian@ganteng.eu>
2022-07-21 11:30:26 +0700

    use pm2

--- README.md
+++ README.md
@@ -1,39 +1,57 @@
 # tgnodeWRT
+
 Personal telegram bot for openwrt
 
 # Usage
+
 ## Install NodeJS
+
 ```bash
 opkg update
-opkg install node node-npm vnstat vnstati
+opkg install node node-npm vnstat vnstati git git-http
+```
+
+## Install Nodejs Package
+
+```bash
+npm i -g yarn pm2
 ```
 
 ## Install bot
+
 1. Clone this repository
-    ```bash
-    git clone https://github.com/ardcsx/tgnodewrt.git
-    ```
-2. Install dependecies (`yarn` or `npm install`)
-3. edit ```.env``` file, change ```TOKEN``` and ```ADMIN_ID```
+   ```bash
+   git clone https://github.com/ardcsx/tgnodewrt.git
+   ```
+2. Install dependencies (`yarn` or `npm install`)
+3. edit `.env` file, change `TOKEN` and `ADMIN_ID`
 4. Start the development environment
-    ```bash
-    npm run dev or yarn dev
-    ```
+   ```bash
+   npm run dev or yarn dev
+   ```
+
 ### Auto start on boot
+
 ```bash
 cp /root/tgnodewrt/etc/init.d/tgnodewrt /etc/init.d/
 /etc/init.d/tgnodewrt enable
 /etc/init.d/tgnodewrt start
 ```
+
+### Crontab setting
+
+```bash
+/root/tgnodewrt/bot.sh
+Telegram NodeJS Openwrt Menu
+
+1) add bot crontab        3) add speedtest crontab  5) install bot
+2) del bot crontab        4) del speedtest crontab  6) Quit
+```
+
 ### Speedtest every 1 hour
+
 Add to scheduled task
+
 ```bash
 0 */1 * * *  /root/tgnodewrt/command/sh/speed.sh
 ```
-
-
-
-
-
-
-
--- bot.sh
+++ bot.sh
@@ -3,11 +3,11 @@
 
 echo -e "Telegram NodeJS Openwrt Menu\n"
 PS3='Please enter your choice: '
-options=("add crontab" "del crontab" "install bot" "Quit")
+options=("add bot crontab" "del bot crontab" "add speedtest crontab" "del speedtest crontab" "install bot" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "add crontab")
+        "add bot crontab")
             tmpfile=$(mktemp)
             crontab -l >"$tmpfile"
             printf '%s\n' "*/2 * * * *  /root/tgnodewrt/util/bot-checker.sh" >>"$tmpfile"
@@ -15,12 +15,28 @@ do
             echo -e "Successfully add crontab"
             break
             ;;
-        "del crontab")
+        "del bot crontab")
             tmpfile=$(mktemp)
             crontab -l >"$tmpfile"
             sed -i "/bot-checker.sh/d" "$tmpfile"
             crontab "$tmpfile" && rm -f "$tmpfile"
             echo "Successfully delete crontab"
+            break
+            ;;
+        "add speedtest crontab")
+            tmpfile=$(mktemp)
+            crontab -l >"$tmpfile"
+            printf '%s\n' "0 */1 * * *  /root/tgnodewrt/command/sh/speed.sh" >>"$tmpfile"
+            crontab "$tmpfile" && rm -f "$tmpfile"
+            echo -e "Successfully add speedtest crontab"
+            break
+            ;;
+        "del speedtest crontab")
+            tmpfile=$(mktemp)
+            crontab -l >"$tmpfile"
+            sed -i "/speed.sh/d" "$tmpfile"
+            crontab "$tmpfile" && rm -f "$tmpfile"
+            echo "Successfully delete speedtest crontab"
             break
             ;;
         "install bot")
