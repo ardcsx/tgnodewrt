13d9a28a8a4fcc446daff45803ff447fe35e2cb8
ardcs <ardian@ganteng.eu>
2022-12-26 11:16:24 +0700

    add OpenAi API for ask command

---
+++ command/ask.js
@@ -0,0 +1,39 @@
+import { Configuration, OpenAIApi } from "openai";
+import { Composer } from "telegraf";
+import { editMsg } from "../lib/helper.js";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const composer = new Composer();
+
+async function handleAsk(ctx) {
+  try {
+    const reply = await ctx.reply("Please wait", {
+      reply_to_message_id: ctx.message.message_id,
+    });
+    ctx.replyWithChatAction("typing");
+    const question = ctx.match[1];
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: question,
+      max_tokens: 1000,
+      temperature: 0,
+    });
+    if (completion.status === 200) {
+      editMsg(ctx, reply, completion.data.choices[0].text);
+    } else {
+      console.log(completion);
+      editMsg(ctx, reply, "Error with OpenAI API request");
+    }
+    return;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+composer.hears(/^.ask (.+)/i, handleAsk);
+
+export default composer;
--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-import { Telegraf } from "telegraf"
-import 'dotenv/config'
+import { Telegraf } from "telegraf";
+import "dotenv/config";
 
 import modeminfo from "./command/modeminfo.js";
 import my_ip from "./command/my_ip.js";
 import bash_cmd from "./command/bash_cmd.js";
 import speedtest from "./command/speedtest.js";
-import SystemHandler from "./command/system.js"
-import vnstat from "./command/vnstat.js"
-import tts from "./command/tts.js"
-import sidompulHandler from "./command/sidompul.js"
-import customCommand from "./command/custom_cmd.js"
-import helpHandler from "./command/help.js"
-import speedtestlog from "./command/speedtest_log.js"
+import SystemHandler from "./command/system.js";
+import vnstat from "./command/vnstat.js";
+import tts from "./command/tts.js";
+import sidompulHandler from "./command/sidompul.js";
+import customCommand from "./command/custom_cmd.js";
+import helpHandler from "./command/help.js";
+import speedtestlog from "./command/speedtest_log.js";
+import askHandler from "./command/ask.js";
 import { isAdmin } from "./lib/helper.js";
 
-
-
-const bot = new Telegraf(process.env.TOKEN)
+const bot = new Telegraf(process.env.TOKEN);
 
 bot.telegram.getMe().then((bot_informations) => {
-    bot.options.username = bot_informations.username;
-    console.log("Server has initialized bot nickname. Nick: " + bot_informations.username);
+  bot.options.username = bot_informations.username;
+  console.log(
+    "Server has initialized bot nickname. Nick: " + bot_informations.username
+  );
 });
 
+bot.use(isAdmin);
+bot.use(helpHandler);
+bot.use(SystemHandler);
+bot.command("modeminfo", modeminfo);
+bot.command("my_ip", my_ip);
+bot.hears(/^.bash (.+)/i, bash_cmd);
+bot.use(vnstat);
+bot.use(tts);
+bot.use(sidompulHandler);
+bot.use(customCommand);
+bot.use(speedtest);
+bot.use(speedtestlog);
+bot.use(askHandler);
 
-bot.use(isAdmin)
-bot.use(helpHandler)
-bot.use(SystemHandler)
-bot.command('modeminfo', modeminfo)
-bot.command('my_ip', my_ip)
-bot.hears(/^.bash (.+)/i, bash_cmd)
-bot.use(vnstat)
-bot.use(tts)
-bot.use(sidompulHandler)
-bot.use(customCommand)
-bot.use(speedtest)
-bot.use(speedtestlog)
-
-bot.launch()
+bot.launch();
